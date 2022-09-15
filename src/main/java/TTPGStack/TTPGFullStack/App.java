9b5366ed946b298f6ae71de66cbd852bf62ee37f
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-09-15 11:44:09 +0530

    Welcome to java stack

---
+++ src/main/java/TTPGStack/TTPGFullStack/App.java
@@ -0,0 +1,13 @@
+package TTPGStack.TTPGFullStack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/TTPGStack/TTPGFullStack/ScannerClass.java
@@ -0,0 +1,17 @@
+package TTPGStack.TTPGFullStack;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Welocome to java full stack");
+		System.out.println("Procedures in java");
+		String process=scan.next();
+		System.out.println(process);
+
+	}
+
+}
