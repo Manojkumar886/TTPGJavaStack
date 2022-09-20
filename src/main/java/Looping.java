89064fd3fe50b2aae9dc94a2462583b77d890bcb
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-09-20 12:04:44 +0530

    Loopin is Over

---
+++ src/main/java/Looping.java
@@ -0,0 +1,67 @@
+
+public class Looping {
+
+	public static void main(String[] args) 
+	{
+		
+//		for(int i=1;i<=5;i++)
+//		{
+//			System.out.println("Praveen");
+//		}
+//		int a=15;
+//		while(a<=10)
+//		{
+//			System.out.println(a);
+//			a++;
+//		}
+		int pos=2;
+		do
+		{
+			System.out.println("Welcome do-while looping");
+			pos++;
+		}
+		while(pos<=1);
+	}
+
+}
+
+
+
+//
+//start-initialization(1)
+//stop- condition(50)
+//each step- increment/decrement(+1)
+//
+//
+//Java Looping Types:
+//1.For Loop
+//2.While Loop-Entry Loop
+//3.Do While Loop-Exit Loop
+//
+//
+//FOR LOOP:
+//	syntax:
+//		for(initialization;condition;incre/decre)
+//		{
+//			Statement
+//		}
+//
+//WHILE LOOP:
+//	syntax:
+//		initiazation
+//		while(condition)
+//		{
+//			Statement;
+//			incre/decre;
+//		}
+//
+//DO WHILE LOOP:
+//	syantax:
+//			init;
+//		do
+//		{
+//			statement;
+//			incre/decre;
+//		}
+//		while(condition);
+
