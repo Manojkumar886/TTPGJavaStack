50865cc757af1da36d5831ef85d7dfae1a371bb4
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-10-23 16:48:32 +0530

    Thread is Over

---
+++ src/main/java/MultiThreading/Synchronized.java
@@ -0,0 +1,45 @@
+package MultiThreading;
+
+
+class synch1
+{
+	synchronized public  void printable(int add)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println(add*i);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
+public class Synchronized 
+{
+	public static void main(String[] args) 
+	{
+		final synch1 sy=new synch1();
+		Thread th=new Thread()
+		{
+			public void run()
+			{
+				sy.printable(5);
+			}	
+		};
+		Thread th1=new Thread()
+		{
+			public void run()
+			{
+				sy.printable(7);
+			}	
+		};
+		
+		th.start();
+		th1.start();
+	
+	}
+
+}
