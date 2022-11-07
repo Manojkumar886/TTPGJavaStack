8e81e43084116637c90ae9b5588be1fd711aca5e
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-11-07 15:00:39 +0530

    Search,Delete and the update is completed

--- src/main/java/ConsoleApplication/Connection.java
+++ src/main/java/ConsoleApplication/Connection.java
@@ -11,7 +11,7 @@ public class Connection implements ZealousAcademyActions
 	{
 		academy[0]=new ZealousAcademy("Manojkumar","JavaFullStack", "Razak", 2,2.00 , 20000.0);
 		academy[1]=new ZealousAcademy("Thivin","PythonFullStack", "Annamalai", 4,1.00 , 35000.0);
-		academy[2]=new ZealousAcademy("Thilak","JavaWebDevelopment", "Raseedha", 5,2.40 , 10000.0);
+		academy[2]=new ZealousAcademy("Thilak","JavaFullStack", "Raseedha", 5,2.40 , 10000.0);
 		academy[3]=new ZealousAcademy("Vishnu","C#", "Manojkumar", 4,1.30 , 14000.0);
 		academy[4]=new ZealousAcademy("Ganesh","Testing", "Aarthi", 10,1.10 , 35000.0);
 		academy[5]=new ZealousAcademy("Pradeep","PHP", "Sabari", 1,2.30 , 20000.0);
@@ -39,8 +39,23 @@ public class Connection implements ZealousAcademyActions
 				System.out.println("your values is listed");
 				connect.Listallstudentdetails();
 				break;
+			case 3:
+				System.out.println("which student name you want update");
+				String stud=connect.scan.next();
+				connect.updateStudentdetails(stud);
+				break;
+			case 4:
+				System.out.println("search based on technolgy");
+				System.out.println("which technology you want search");
+				String tech=connect.scan.next();
+				connect.searchStduentdetails(tech);
+				break;
 			case 5:
 				System.out.println("your values is sorted");
+			case 6:
+				System.out.println("which name you want to delete in academic");
+				String nm=connect.scan.next();
+				connect.DeleteStduentdetails(nm);
 			}
 			
 			
@@ -73,15 +88,43 @@ public class Connection implements ZealousAcademyActions
 	}
 
 	@Override
-	public void updateStudentdetails(String name) {
-		// TODO Auto-generated method stub
+	public void updateStudentdetails(String name) 
+	{
+		for(int index=0;index<academy.length;index++)
+		{
+			if(academy[index].getStudentName().equalsIgnoreCase(name))
+			{
+				System.out.println(name+"which field you want to update");
+				System.out.println(academy[index]);
+				//System.out.println("that fields are studentname,inchargename,members");
+				String field=scan.next();
+				switch(field)
+				{
+				case "studentname":
+					System.out.println("try to youe new student name");
+					String newstud=scan.next();
+					academy[index].setStudentName(newstud);
+					break;
+				}
+				System.out.println(field+"has been updated");	
+			}
+			
+		}
 		
 	}
 
 	@Override
-	public void searchStduentdetails(String technology) {
-		// TODO Auto-generated method stub
-		
+	public void searchStduentdetails(String technology) 
+	{
+	System.out.println("i have trying to serach for technologies"+technology);
+	for(int index=0;index<academy.length;index++)
+	{
+		if(academy[index].getStudentTechnology().equalsIgnoreCase(technology))
+		{
+			System.out.println(academy[index]);
+		}
+	}
+	return;
 	}
 
 	@Override
@@ -94,9 +137,18 @@ public class Connection implements ZealousAcademyActions
 	}
 
 	@Override
-	public void DeleteStduentdetails(String name) {
-		// TODO Auto-generated method stub
-		
+	public void DeleteStduentdetails(String name)
+	{
+		for(int index=0;index<academy.length;index++)
+		{
+			if(academy[index].getStudentName().equalsIgnoreCase(name))
+			{
+				academy[index]=null;
+				System.out.println(name+"has been deleted succesfully");
+				return;
+			}
+		}
+		System.out.println(name+"has't deleted....");
 	}
 
 }
